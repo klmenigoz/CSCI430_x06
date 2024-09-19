e5bb40aa27ee995d5953dbc26f2ffa588c2a0f9a
Kent Menigoz <klmenigoz@csuchico.edu>
2024-09-18 20:22:57 -0700

    removed bug to see if CI badge goes green

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
     cout << "Hello World!\n";   // no errors here
-        cout << "Hallo Welt?\n" // no semicolon, actions should catch this on github
+        cout << "Hallo Welt?\n"; // previously a bug here for testing CI badge, lacking semicolon
     
   return 0;
 }
