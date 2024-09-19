9279b1e31f394f050dbb3a314f3d60f50c068e29
Kent Menigoz <klmenigoz@csuchico.edu>
2024-09-18 19:16:49 -0700

    turned main into a hello world, one works, actions should detect error after the german one

--- FirstIO-main/main.cpp
+++ FirstIO-main/main.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main(){
-  
-    cout << "Hello World!"
+
+    cout << "Hello World!\n";   // no errors here
+        cout << "Hallo Welt?\n" // no semicolon, actions should catch this on github
     
   return 0;
 }
